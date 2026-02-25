1ef038249e797cfd71cd953d3bbdb18f7ef25deb
seb-creator01 <precioussebastian70@gmail.com>
2026-02-25 17:33:36 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'slit-hub-v1';
+const assets = [
+  '/',
+  '/index.html',
+  // Add your CSS and JS file paths here
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
